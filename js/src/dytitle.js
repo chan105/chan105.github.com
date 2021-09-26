80d40f9d42ea1107e06de4d3f6b035f61982aac6
chan105 <929647969@qq.com>
2021-09-26 17:18:56 +0800

    Site updated: 2021-09-26 17:18:46

--- js/src/dytitle.js
+++ js/src/dytitle.js
@@ -1,14 +1,14 @@
-<!--卖萌-->
+// 卖萌
 var OriginTitile = document.title;
 var titleTime;
 document.addEventListener('visibilitychange', function () {
     if (document.hidden) {
-        $('[rel="icon"]').attr('href', "/img/TEP.ico");
+        // $('[rel="icon"]').attr('href', "/img/TEP.ico");
         document.title = ' 别离开我 ';
         clearTimeout(titleTime);
     }
     else {
-        $('[rel="icon"]').attr('href', "/favicon.ico");
+        // $('[rel="icon"]').attr('href', "/favicon.ico");
         document.title = ' 我就知道你会回来的 ';
         titleTime = setTimeout(function () {
             document.title = OriginTitile;
